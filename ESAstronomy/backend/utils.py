bc27c456c7962a93a82a438a5ff691fd7fdaf3a5
FabioDiCeglie <fabio.diceglie@freshbooks.com>
2025-07-14 15:03:13 +0200

    feat: add Elasticsearch client utility function

---
+++ ESAstronomy/backend/utils.py
@@ -0,0 +1,18 @@
+import time
+
+from elasticsearch import Elasticsearch
+
+
+def get_es_client(max_retries: int = 5, sleep_time: int = 5) -> Elasticsearch:
+    i = 0
+    while i < max_retries:
+        try:
+            es = Elasticsearch('http://localhost:9200')
+            print("Connected to Elasticsearch")
+            print(f"Cluster info: \n {es.info()}")
+            return es
+        except Exception as e:
+            print(f"Connection failed: {e}")
+            time.sleep(sleep_time)
+            i += 1
+    return ConnectionError("Elasticsearch connection failed")
